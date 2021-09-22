3c254dda278d383e9bcd723806d01aa6407405e8
Janani15197 <91132156+Janani15197@users.noreply.github.com>
2021-09-22 12:26:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser=require('body-parser')
 const cors=require('cors')
 
 
-const PORT =3000
+//const PORT =3000
+const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const api =require('./routes/api')
 const app =express()
 
